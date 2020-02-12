2857ab57c28c95178c08e779d1106debddd3aa2d
Mikey Williams <manicdesigner@gmail.com>
2020-02-12 16:41:13 -0500

    post added

--- server.js
+++ server.js
@@ -23,4 +23,30 @@ server.get('/api/users', async (req, res) => {
 	}
 });
 
+server.post('/api/users', async (req, res) => {
+	if (!req.body.name) {
+		return res
+			.status(400)
+			.json({ message: 'please provide name for the user' });
+	}
+	if (!req.body.bio) {
+		return res.status(400).json({ message: 'please provide bio for the user' });
+	}
+	const newUser = {
+		name: req.body.name,
+		bio: req.body.bio
+	};
+	try {
+		const addUser = await db.insert(newUser);
+		if (addUser) {
+			res.status(201).json(addUser);
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			errorMessage: 'There was an error while saving the user to the database.'
+		});
+	}
+});
+
 module.exports = server;
