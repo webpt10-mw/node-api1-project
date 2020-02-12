22d56d2e79911dcdd91367e12050ebfbe68ffced
Mikey Williams <manicdesigner@gmail.com>
2020-02-12 16:23:00 -0500

    add users get

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const db = require('./data/db');
 
 const server = express();
 
 server.use(express.json());
 
-server.use('/', (req, res) => {
+server.get('/', (req, res) => {
 	res.send('users is up');
+});
+
+server.get('/api/users', async (req, res) => {
+	try {
+		const users = await db.find();
+		if (users) {
+			res.status(200).json(users);
+		}
+	} catch (err) {
+		console.log(err);
+		res
+			.status(500)
+			.json({ errorMessage: 'The users information could not be retrived.' });
+	}
 });
 
 module.exports = server;
