36204753ef8224cfe576847be9b059b388ad8afa
Mikey Williams <manicdesigner@gmail.com>
2020-02-12 17:05:51 -0500

    get user by id

--- server.js
+++ server.js
@@ -49,4 +49,23 @@ server.post('/api/users', async (req, res) => {
 	}
 });
 
+server.get('/api/users/:id', async (req, res) => {
+	const id = req.params.id;
+	try {
+		const user = await db.findById(id);
+		if (user) {
+			res.status(200).json(user);
+		} else {
+			res
+				.status(404)
+				.json({ message: 'The user with the specified ID does not exist' });
+		}
+	} catch (err) {
+		console.log(err);
+		res
+			.status(500)
+			.json({ errorMessage: 'The user information could not be retrieved' });
+	}
+});
+
 module.exports = server;
