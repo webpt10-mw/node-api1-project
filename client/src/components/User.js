1737e5e54d6f128aa9c2255f631e418bfe84b284
Michael Williams <manicdesigner@gmail.com>
2020-02-18 15:52:21 -0500

    add classnames

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -3,9 +3,11 @@ import React, { useState } from 'react';
 function User(props) {
   const { name, bio } = props.info;
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>{bio}</p>
+    <div className='user-display-box'>
+      <div className='info-display'>
+        <h2>{name}</h2>
+        <p>{bio}</p>
+      </div>
     </div>
   );
 }
